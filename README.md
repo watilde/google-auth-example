6c3e1c631f1b996f45058ef5225fd11b262a1275
Daijiro Wachi <daijiro.wachi@gmail.com>
2018-06-10 20:24:41 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Google auth example
+
+## Getting started
+1. Register your app on https://console.developers.google.com
+1. Rename `example.config.json` to `config.json`
+2. Fill your tokens in config.json
+3. `npm i && npm start`
+4. Open http://localhost:3000
+
+## Errors?
++ https://github.com/jaredhanson/passport-google-oauth/issues/76#issuecomment-396014572
---
+++ app.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const session = require('express-session')
+const cookieParser = require('cookie-parser')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const config = require('./config.json')
+
+passport.serializeUser((user, done) => {
+  done(null, user)
+})
+
+passport.deserializeUser((obj, done) => {
+  done(null, obj)
+})
+
+passport.use(new GoogleStrategy(
+  config.google,
+  (accessToken, refreshToken, profile, done) => {
+    return done(null, profile)
+  }
+))
+
+const app = express()
+app.use(cookieParser())
+
+// @see https://www.npmjs.com/package/express-session
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static(__dirname + '/public'));
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email profile'] }))
+
+app.get('/auth/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login.html' }),
+  (req, res) => { res.redirect('/') }
+)
+
+app.get('/api/auth', function(req, res) {
+  console.log(req)
+  res.json(req.user)
+})
+
+app.get('/logout', function(req, res) {
+  req.logout()
+  res.redirect('/')
+})
+
+app.listen(config.port)
